4dacee003e0199aa227ea689aa4b07f3fd12f178
Anh Dao <frankensteinruo@gmail.com>
2017-08-28 00:41:14 +1000

    Added a birthday picker.

--- src/app/views/protected/profile/details/index.js
+++ src/app/views/protected/profile/details/index.js
@@ -1,1 +1,67 @@
-import * from './Details'
+import React, { PureComponent, } from 'react'
+import {
+  ControlLabel,
+  FormGroup,
+  FormControl,
+} from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
+
+class Details extends PureComponent {
+
+  state = {
+    email: '',
+    name: '',
+    startDate: moment(),
+  };
+
+  constructor(props) {
+    super(props)
+
+  }
+
+  handleNameChange = (e) => {
+    this.setState({ name: e.target.value, })
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({ email: e.target.value, })
+  }
+
+  handleBirthdayChange = (newDate) => {
+    this.setState({ startDate: newDate, })
+  }
+
+  render() {
+    return (
+      <section>
+        <FormGroup>
+          <ControlLabel>Your Name</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.name}
+            placeholder="Enter your name"
+            onChange={this.handleNameChange}
+          />
+          <ControlLabel>Your Email</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.email}
+            placeholder="Enter your email"
+            onChange={this.handleEmailChange}
+          />
+          <ControlLabel>Your Day of Birth</ControlLabel>
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleBirthdayChange}
+          />
+        </FormGroup>
+      </section>
+    )
+  }
+}
+
+export {
+  Details,
+}
--- src/app/views/protected/profile/location/index.js
+++ src/app/views/protected/profile/location/index.js
@@ -1,1 +1,15 @@
-import * from './Location'
+import React, { PureComponent, } from 'react'
+
+class Location extends PureComponent {
+  render() {
+    return (
+      <section>
+        <h1>Map view is here</h1>
+      </section>
+    )
+  }
+}
+
+export {
+  Location,
+}
