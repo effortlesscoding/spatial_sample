004f0c511e0517ed0176f9ec3c1eb8f676e74a8b
Anh Dao <frankensteinruo@gmail.com>
2017-08-28 01:34:29 +1000

    Added Google Maps.

--- src/app/views/protected/profile/location/index.js
+++ src/app/views/protected/profile/location/index.js
@@ -1,10 +1,45 @@
 import React, { PureComponent, } from 'react'
+import { ControlLabel, } from 'react-bootstrap'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng, } from 'react-places-autocomplete'
+import { GoogleMap, Marker } from "react-google-maps";
+import { LocationMap, } from './locationmap'
+import _ from 'lodash'
 
 class Location extends PureComponent {
+  state = {
+    location: '',
+  }
+
+  _handleLocationChage = (location) => {
+    this.setState({ location, })
+  }
+
+  _handleMapClick = () => {
+    console.log('_handleMapClick')
+  }
+
   render() {
     return (
       <section>
-        <h1>Map view is here</h1>
+        <ControlLabel>Your Location</ControlLabel>
+        <PlacesAutocomplete
+          inputProps={{
+            value: this.state.location,
+            onChange: this._handleLocationChage,
+          }}
+        />
+        <LocationMap
+          containerElement={
+            <div style={{ zIndex: -1, height: `500px`, width: '100%', }} />
+          }
+          mapElement={
+            <div style={{ zIndex: -1, height: `500px`, width: '100%', }} />
+          }
+          onMapLoad={_.noop}
+          onMapClick={_.noop}
+          markers={[]}
+          onMarkerRightClick={_.noop}
+        />
       </section>
     )
   }
